07a5883991bfe04c5d7daa768ef4a79a76329853
Voller Silas <voller.silas@gmail.com>
2022-07-27 13:00:01 -0300

    Quarta tela- Entendi errado que ela seria um bônus - Requisito layout completo

---
+++ src/components/FourthScreen.js
@@ -0,0 +1,35 @@
+const FourthScreen = () => {
+    return (
+        <div class="fourth-screen hidden">
+            <div class="order-status">
+                <h1>Pedido feito <br/> com sucesso!</h1>
+            </div>
+
+            <div class="film-section">
+                <h2>Filme e sessão</h2>
+                <p>Enola Holmes</p>
+                <p>24/06/2021 15:00</p>
+            </div>
+
+            <div class="tickets">
+                <h2> Ingressos</h2>
+                <p>Assento 15</p>
+                <p>Assento 16</p>
+            </div>
+
+            <div class="buyers">
+                <h2>Comprador</h2>
+                <p>Nome: João da Silva Sauro</p>
+                <p>CPF: 121.456.789.-10</p>
+            </div>
+
+            <div class="home">
+                <button>Voltar para home</button>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default FourthScreen

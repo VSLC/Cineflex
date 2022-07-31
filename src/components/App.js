55e63a98e356b111abc727bc2be3b1cd07732d69
voller <voller.silas@gmail.com>
2022-07-31 10:34:18 -0300

    últimos ajustes App

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,25 @@
-import InitialScreen from "./initialscreen/InitialScreen"
-import SecondScreen from "./secondscreen/SecondScreen"
-import ThirdScreen from "./thirdscreen/ThirdScreen"
-import FourthScreen from "./fourthscreen/FourthScreen"
-import Header from "./Header"
+import InitialScreen from "./initialscreen/InitialScreen";
+import SecondScreen from "./secondscreen/SecondScreen";
+import ThirdScreen from "./thirdscreen/ThirdScreen";
+import FourthScreen from "./fourthscreen/FourthScreen";
+import Header from "./Header";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-    return (
-        <>
-            <Header />
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<InitialScreen />} />
-                    <Route path="/sessoes/:idFilme" element={<SecondScreen />} />
-                    <Route path="/assentos/:idSessao" element={<ThirdScreen />} />
-                    <Route path="/sucesso" element={<FourthScreen />} />
-                </Routes>
+  return (
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<InitialScreen />} />
+          <Route path="/sessoes/:idFilme" element={<SecondScreen />} />
+          <Route path="/assentos/:idSessao" element={<ThirdScreen />} />
+          <Route path="/sucesso" element={<FourthScreen />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-            </BrowserRouter>
-        </>
-    );
-}
-
-export default App
+export default App;
